901047f81a56e6906a7047f1a0393e90dfc5716e
hajin <enginhajin@gmail.com>
2024-10-11 11:59:10 +0900

    [Bronze III] Title: 팩토리얼, Time: 88 ms, Memory: 9608 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.js
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.js
@@ -1,13 +1,12 @@
 const fs = require("fs");
 const num = Number(fs.readFileSync("/dev/stdin").toString());
 
-const factorial = (num) => {
-  if (num === 0) return 1;
-  let result = 1;
-  for (let i = 1; i <= num; i++) {
-    result *= i;
-  }
-  return result;
-};
+const factorial = (num) =>
+  num === 0
+    ? 1
+    : Array.from({ length: num }, (_, i) => i + 1).reduce(
+        (acc, curr) => acc * curr,
+        1
+      );
 
 console.log(factorial(num));
