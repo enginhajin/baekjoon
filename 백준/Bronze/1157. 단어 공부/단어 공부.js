1db0437aff149c2ee5114f35169e6ed4f7f278db
hajin <enginhajin@gmail.com>
2024-11-21 23:51:44 +0900

    [Bronze I] Title: 단어 공부, Time: 204 ms, Memory: 16620 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.js
+++ 백준/Bronze/1157. 단어 공부/단어 공부.js
@@ -2,18 +2,25 @@ const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().toUpperCase();
 
 const counts = new Array(26).fill(0);
+let max = 0;
+let maxCount = 0;
+let maxIndex = -1;
 
 for (const char of input) {
   const idx = char.charCodeAt(0) - 65;
   counts[idx]++;
-}
 
-const max = Math.max(...counts);
-const maxCount = counts.filter((item) => item === max).length;
+  if (counts[idx] > max) {
+    max = counts[idx];
+    maxCount = 1;
+    maxIndex = idx;
+  } else if (counts[idx] === max) {
+    maxCount++;
+  }
+}
 
 if (maxCount > 1) {
   console.log("?");
 } else {
-  const idx = counts.indexOf(max);
-  console.log(String.fromCharCode(idx + 65));
+  console.log(String.fromCharCode(maxIndex + 65));
 }
