7600e70fc2e1609d648da6c61573f23ef0d40e33
hajin <enginhajin@gmail.com>
2024-11-21 11:20:06 +0900

    [Bronze III] Title: 오븐 시계, Time: 96 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.js
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.js
@@ -4,15 +4,9 @@ const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 const [A, B] = input[0].split(" ").map(Number);
 const C = Number(input[1]);
 
-let hour = A;
-let minute = B + C;
+const totalMinutes = A * 60 + B + C;
 
-if (minute >= 60) {
-  hour = hour + Math.floor(minute / 60);
-  minute = minute % 60;
-  if (hour >= 24) {
-    hour = hour % 24;
-  }
-}
+const hour = Math.floor(totalMinutes / 60) % 24;
+const minute = totalMinutes % 60;
 
 console.log(hour, minute);
