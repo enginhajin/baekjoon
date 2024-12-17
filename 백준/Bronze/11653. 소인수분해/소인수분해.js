a29cdc7b1a10c077d77a2234b99cf720ae5a8af3
hajin <enginhajin@gmail.com>
2024-12-17 19:47:12 +0900

    [Bronze I] Title: 소인수분해, Time: 116 ms, Memory: 9616 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.js
+++ 백준/Bronze/11653. 소인수분해/소인수분해.js
@@ -4,12 +4,14 @@ let N = Number(fs.readFileSync("/dev/stdin"));
 const factors = [];
 let divisor = 2;
 
-while (N > 1) {
+while (divisor <= Math.sqrt(N)) {
   while (N % divisor === 0) {
     factors.push(divisor);
     N /= divisor;
   }
   divisor++;
 }
+
+if (N > 1) factors.push(N);
 
 console.log(factors.join("\n"));
