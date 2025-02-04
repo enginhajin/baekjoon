12324726f3f80e7ef66c99b94001ba36c14b5fea
hajin <enginhajin@gmail.com>
2025-02-04 23:14:34 +0900

    [Silver IV] Title: 제로, Time: 180 ms, Memory: 17572 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const data = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .splice(1)
+  .map(Number);
+
+const stack = [];
+
+data.forEach((item) => {
+  if (item === 0) {
+    stack.pop();
+  } else {
+    stack.push(item);
+  }
+});
+
+console.log(stack.length ? stack.reduce((acc, curr) => (acc += curr)) : 0);
